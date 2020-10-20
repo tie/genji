4fa272460533962e16355526bdb5fc799596143a
Ivan Trubach <mr.trubach@icloud.com>
2020-10-21 02:27:54 +0300

    Breaking more things

--- .github/workflows/scripts/setup-env.js
+++ .github/workflows/scripts/setup-env.js
@@ -4,45 +4,69 @@ const https = require('https')
 const path = require('path')
 
 module.exports = async ({ core }) => {
-  // Get Go release tags.
+  const {
+    goos, goarch, goarm,
+    goroot, gopath,
+    releaseTags,
+    releaseRef,
+    buildCache,
+    modCache,
+  } = await core.group('Gather environment information', async () => {
+    const [goos, goarch, goarm] = process.env.platform.split('/')
+    core.info(`Setting up for GOOS=${goos} GOARCH=${goarch}` + (goarm ? ' GOARM='+goarm : ''))
 
-  const releaseTags = execFileSync('go', [
-    'list', '-f',
-    '{{range context.ReleaseTags}}{{println .}}{{end}}',
-    'runtime',
-  ]).toString().trim().split(/\r?\n/)
+    // Get some standard paths.
+    const [goroot, gopath] = execFileSync('go', [
+      'env', 'GOROOT', 'GOPATH',
+    ]).toString().trim().split(/\r?\n/)
 
-  // Set up Go environment variables.
+    // Get Go release tags.
+    const releaseTags = execFileSync('go', [
+      'list', '-f',
+      '{{range context.ReleaseTags}}{{println .}}{{end}}',
+      'runtime',
+    ]).toString().trim().split(/\r?\n/)
 
-  const [goos, goarch] = process.env.platform.split('/')
-  core.exportVariable('GOOS', goos)
-  core.exportVariable('GOARCH', goarch)
+    // Get Git tag or sha for Go release.
+    const releaseRef = (() => {
+      const versionFields = execFileSync('go', [
+        'version',
+      ]).toString().trim().split(' ').slice(2, -1)
 
-  core.exportVariable('GOFLAGS', '-race -trimpath -mod=readonly')
+      // ['go1.14.9']
+      if (versionFields.length == 1) {
+        return versionFields[0]
+      }
+      // ['devel', '+66e66e7113', 'Sun', 'Sep', '13', '19:17:09', '2020', '+0000']
+      if (versionFields.length == 8) {
+        return versionFields[1].slice(1)
+      }
+      // Unexpected format, default to latest release tag.
+      return releaseTags[releaseTags.length-1]
+    })()
 
-  // Enable Cgo explicitly for -race flag since we are cross compiling.
+    // Get Go cache paths.
+    const buildCache = execFileSync('go', ['env', 'GOCACHE']).toString().trim()
+    const modCache = (() => {
+      if (releaseTags.includes('go1.15')) {
+        return execFileSync('go', ['env', 'GOMODCACHE']).toString().trim()
+      } else {
+        return path.join(gopath, 'pkg', 'mod')
+      }
+    })()
 
-  core.exportVariable('CGO_ENABLED', '1')
-  core.exportVariable('CC', 'powerpc64le-linux-gnu-gcc')
-
-  // Export cache paths for further steps.
-
-  const buildCache = execFileSync('go', ['env', 'GOCACHE']).toString().trim()
-  const modCache = (() => {
-    if (releaseTags.includes('go1.15')) {
-      return execFileSync('go', ['env', 'GOMODCACHE']).toString().trim()
-    } else {
-      const gopath = execFileSync('go', ['env', 'GOPATH']).toString().trim()
-      return path.join(gopath, 'pkg', 'mod')
+    return {
+      goos, goarch, goarm,
+      goroot, gopath,
+      releaseTags,
+      releaseRef,
+      buildCache,
+      modCache,
     }
-  })()
-
-  core.exportVariable('GOCACHE', buildCache)
-  core.exportVariable('GOMODCACHE', modCache)
+  })
 
   await core.group('Run apt install', async () => {
-    // Install QEMU and GCC cross compiler.
-
+    // Install QEMU (and GCC cross compiler).
     execFileSync('sudo', [
       'apt-get', 'install',
       '--no-install-recommends', '-y',
@@ -56,37 +80,13 @@ module.exports = async ({ core }) => {
     })
   })
 
-  // Pass target sysroot and program loader path to QEMU.
-
-  core.exportVariable('QEMU_LD_PREFIX', '/usr/powerpc64le-linux-gnu')
-
-  // Prebuilt Go releases do not contain race detector runtime for non-host architectures.
-  // Download syso object for the latest known **major** release and place it in GOROOT.
-
   await core.group('Download race detector runtime', async () => {
-    const sysoRef = (() => {
-      // See https://github.com/golang/go/issues/42080
-      if (releaseTags.includes('go1.15') && goos == 'linux' && goarch == 'ppc64le') {
-        return 'go1.15rc1'
-      }
+    // Prebuilt Go releases do not contain race detector runtime for non-host architectures.
+    // Download syso object from Git repo and place it in GOROOT.
 
-      const versionFields = execFileSync('go', [
-        'version',
-      ]).toString().trim().split(' ').slice(2, -1)
-
-      // ['go1.14.9']
-      if (versionFields.length == 1) {
-        return versionFields[0]
-      }
-      // ['devel', '+66e66e7113', 'Sun', 'Sep', '13', '19:17:09', '2020', '+0000']
-      if (versionFields.length == 8) {
-        return versionFields[1].slice(1)
-      }
-      // Unknown format, default to latest release tag.
-      return releaseTags[releaseTags.length-1]
-    })()
-
-    const goroot = execFileSync('go', ['env', 'GOROOT']).toString().trim()
+    const sysoRef = (releaseTags.includes('go1.15') && goos == 'linux' && goarch == 'ppc64le')
+                  ? 'go1.15rc1' // See https://github.com/golang/go/issues/42080
+                  : releaseRef
 
     const sysoName = `race_${goos}_${goarch}.syso`
     const sysoPath = `src/runtime/race/${sysoName}`
@@ -104,5 +104,28 @@ module.exports = async ({ core }) => {
     }).on('error', (err) => {
       core.setFailed(`download race runtime: ${err}`)
     })
+  })
+
+  await core.group('Export environment variables', async () => {
+    // Set up Go flags.
+    core.exportVariable('GOFLAGS', '-race -trimpath -mod=readonly')
+
+    // Set up Go cross compilation.
+    core.exportVariable('GOOS', goos)
+    core.exportVariable('GOARCH', goarch)
+    if (goarm) {
+      core.exportVariable('GOARM', goarm)
+    }
+
+    // Enable Cgo explicitly for -race flag since we are cross compiling.
+    core.exportVariable('CGO_ENABLED', '1')
+    core.exportVariable('CC', 'powerpc64le-linux-gnu-gcc')
+
+    // Export cache paths for actions/cache step.
+    core.exportVariable('GOCACHE', buildCache)
+    core.exportVariable('GOMODCACHE', modCache)
+
+    // Pass target sysroot and program loader path to QEMU.
+    core.exportVariable('QEMU_LD_PREFIX', '/usr/powerpc64le-linux-gnu')
   })
 }
